9742486d22e0c2b88d7e044e3fa8e1d99994efcb
shashi <ssubramanyam@inlogiq.com>
2018-02-17 17:12:13 +0100

    Initial setup

---
+++ ecart-core/src/main/java/com/natslash/ECartCoreApplication.java
@@ -0,0 +1,12 @@
+package com.natslash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECartCoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ECartCoreApplication.class, args);
+	}
+
+}
---
+++ ecart-core/src/test/java/com/natslash/ECartCoreApplicationTest.java
@@ -0,0 +1,14 @@
+package com.natslash;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ECartCoreApplicationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
