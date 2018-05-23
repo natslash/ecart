857df21d96754a288e7aaddc01d9c1b60f446de3
Shashi Subramanyam <shashi.subramanyam@orenesgrupo.com>
2018-05-23 10:32:33 +0200

    Added test

--- ecart-core/src/test/java/com/natslash/ECartCoreApplicationTest.java
+++ ecart-core/src/test/java/com/natslash/ECartCoreApplicationTest.java
@@ -1,14 +1,34 @@
+/**
+ * Copyright 2018 Orenes Grupo.
+ */
+
 package com.natslash;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 
+/**
+ * 
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(
+    classes = ECartCoreApplication.class)
 public class ECartCoreApplicationTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+  @Autowired
+  DataSource dataSource;
+
+  @Test
+  public void testDummy() throws SQLException {
+    final String schema = dataSource.getConnection().getCatalog();
+    assertEquals("ecart", schema);
+  }
 
 }
